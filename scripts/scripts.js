26e7059a3f25828cb00b8fb9af4ef0cdf88b9c9e
icaraps <icaraps@hotmail.com>
2024-05-22 11:47:40 +0400

    add history url to template preview fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -251,6 +251,6 @@ function createPromiseDialog(textContent = 'Are you sure?', withConfirm = false)
 window.alertDialog = (text = 'ALERT') => createPromiseDialog(text);
 window.confirmDialog = (text = 'Are you sure?') => createPromiseDialog(text, true);
 
-if (window.location.pathname === '/' && (window.location.hash.startsWith('#templates/') || window.sessionStorage.hash.startsWith('#templates/'))) {
+if (window.location.pathname === '/' && (window.location.hash.startsWith('#templates/') || String(window.sessionStorage.hash).startsWith('#templates/'))) {
   document.body.style.display = 'none';
 }
