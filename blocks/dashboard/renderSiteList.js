b3d9be8b334b3e259fef7c57b285f0e35876260d
Fankhauser-Dominik <domi.fankhauser@hotmail.com>
2024-10-11 16:41:26 +0200

    Merge Conflicts resolved

--- blocks/dashboard/renderSiteList.js
+++ blocks/dashboard/renderSiteList.js
@@ -1,125 +1,15 @@
-import {
-  hasDarkAlleyAccess,
-  KESTREL_ONE, OOPS, SCRIPT_API, waitForAuthenticated,
-} from '../../scripts/scripts.js';
+import { waitForAuthenticated, SCRIPT_API, KESTREL_ONE } from '../../scripts/scripts.js';
+import { readQueryParams, removeQueryParams, writeQueryParams } from '../../libs/queryParams/queryParams.js';
+import paginator from '../../libs/pagination/pagination.js';
 
-export default async function renderSites({ container, nav }) {
-  container.innerHTML = '<img src="/icons/loading.svg" alt="loading"/>';
-  nav.innerHTML = '<a href="/" id="create-new-button" title="Create new site" class="button primary action new">Create new site</a>';
-
-  await waitForAuthenticated();
-  const token = await window.auth0Client.getTokenSilently();
-
-  const projectsResponse = await fetch(`${SCRIPT_API}/list`, {
-    headers: {
-      'content-type': 'application/json',
-      authorization: `bearer ${token}`,
-    },
-  }).catch(() => ({ ok: false }));
-
-  if (!projectsResponse.ok) {
-    container.innerHTML = OOPS;
-    return;
-  }
-
-  const { projects, darkAlleyProjects } = await projectsResponse.json().catch(() => ({}));
-
-  if (!projects?.length && !hasDarkAlleyAccess()) {
-    container.innerHTML = '<p>No Sites found</p>';
-    return;
-  }
-
-  container.innerHTML = '';
-
-  // TODO: consolidate DA & Drive projects if we ever commit to both
-  // MARK: List DA
-  if (darkAlleyProjects?.length) {
-    const darkAlleySection = document.createElement('section');
-    darkAlleySection.classList.add('dark-alley-section');
-    darkAlleySection.id = 'dark-alley-section';
-
-    darkAlleySection.innerHTML = `
-      <h3>Dark Alley Sites (Experimental)</h3>
-      <ul>
-        ${darkAlleyProjects
-    .map(
-      ({ projectSlug, projectName, projectDescription }) => `
-          <li>
-            <a href="/da-site/${projectSlug}">
-              <div class="project-thumbnail" data-src="https://${projectSlug}.${KESTREL_ONE}"></div>
-              <div class="project-content">
-                <h2>${projectName}</h2>
-                <p><strong>${projectSlug}</strong></p>
-                <p>${projectDescription || ''}</p>
-              </div>  
-            </a>
-          </li>
-        `,
-    )
-    .join('')}
-      </ul>
-    `;
-
-    container.append(darkAlleySection);
-  }
-
-  // MARK: list drive
-  if (projects?.length) {
-    const sitesSection = document.createElement('section');
-    sitesSection.id = 'google-drive-section';
-    sitesSection.innerHTML = `
-      <h3>Google Drive Sites</h3>
-      <ul id="my-sites-overview">
-        ${projects
-    .map(
-      ({ projectSlug, projectName, projectDescription }) => `
-          <li>
-            <a href="/site/${projectSlug}">
-              <div class="project-thumbnail" data-src="https://${projectSlug}.${KESTREL_ONE}"></div>
-              <div class="project-content">
-                <h2 title="${projectName}">${projectName}</h2>
-                <p title="${projectSlug}"><strong>${projectSlug}</strong></p>
-                <p class="project-description" title="${projectDescription || ''}">${projectDescription || ''}</p>
-              </div>
-            </a>
-          </li>
-        `,
-    )
-    .join('')}
-      </ul>
-    `;
-
-    container.append(sitesSection);
-  }
-
-  // MARK: filter
-  const siteFilter = document.createElement('input');
-  siteFilter.type = 'text';
-  siteFilter.placeholder = 'Filter sites';
-  siteFilter.classList.add('filter-sites', 'filter');
-  container.prepend(siteFilter);
-  siteFilter.addEventListener('input', (event) => {
-    const filter = event?.target?.value?.toLowerCase().trim();
-    if (!filter) {
-      container.querySelectorAll('li[hidden]').forEach((el) => {
-        el.hidden = false;
-      });
-      return;
-    }
-
-    container.querySelectorAll('h2').forEach((el) => {
-      el.closest('li').hidden = !el.textContent.toLowerCase().includes(filter);
-    });
-  });
-
-  // MARK: thumbnails
-  container.querySelectorAll('.project-thumbnail').forEach((thumbnail) => {
+const generateThumbnails = () => {
+  const sitesList = document.querySelector('.sites-list');
+  sitesList.querySelectorAll('.project-thumbnail').forEach((thumbnail) => {
     fetch(thumbnail.dataset.src)
       .then((res) => {
         if (res.ok) {
           return res.text();
         }
-
         return false;
       })
       .then((res) => {
@@ -133,4 +23,100 @@ export default async function renderSites({ container, nav }) {
       })
       .catch(() => null);
   });
+};
+
+async function fetchSites(token, type = 'googleDrive') {
+  let currentPage = readQueryParams().page ? Number(readQueryParams().page) : 1;
+  let currentDaPage = readQueryParams().dapage ? Number(readQueryParams().dapage) : 1;
+  const limit = readQueryParams().limit ? Number(readQueryParams().limit) : 9;
+  const daLimit = readQueryParams().dalimit ? Number(readQueryParams().dalimit) : limit;
+  const searchTerm = readQueryParams().search || '';
+  const isDarkAlley = type === 'darkAlley';
+  const sitesList = document.querySelector(isDarkAlley ? '.sites-list-dark-alley' : '.sites-list-google-drive');
+  const currentLimit = Math.max(1, parseInt(isDarkAlley ? daLimit : limit, 10));
+  const actualPage = isDarkAlley ? currentDaPage : currentPage;
+  const url = `${SCRIPT_API}/${isDarkAlley ? 'darkAlleyList' : 'list'}?searchTerm=${encodeURIComponent(searchTerm)}&limit=${currentLimit}&page=${actualPage}`;
+  fetch(url, {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `bearer ${token}`,
+    },
+  }).then((response) => response.json())
+    .then(({ projects, pagination }) => {
+      if (projects.length === 0) {
+        sitesList.innerHTML = '<p>No Sites found</p>';
+        return;
+      }
+      sitesList.innerHTML = `
+            <h3>${isDarkAlley ? 'Dark Alley Sites (Experimental)' : 'Google Drive Sites'}</h3>
+            <ul id="my-sites-overview">
+              ${projects.map(({ projectSlug, projectName, projectDescription }) => `
+                <li>
+                  <a href="/${isDarkAlley ? 'da-site' : 'site'}/${projectSlug}/overview">
+                    <div class="project-thumbnail" data-src="https://${projectSlug}.${KESTREL_ONE}"></div>
+                    <div class="project-content">
+                      <h2 title="${projectName}">${projectName}</h2>
+                      <p title="${projectSlug}"><strong>${projectSlug}</strong></p>
+                      <p class="project-description" title="${projectDescription || ''}">${projectDescription || ''}</p>
+                    </div>
+                  </a>
+                </li>
+              `).join('')}
+            </ul>
+            ${paginator(pagination.totalItems, currentLimit, actualPage)}
+          `;
+
+      const newSitesList = sitesList.cloneNode(true);
+      // This clears the old event listeners
+      sitesList.replaceWith(newSitesList);
+
+      newSitesList.addEventListener('click', (event) => {
+        const button = event.target.closest('.paginator');
+        if (button) {
+          const newPage = Number(button.getAttribute('data-change-to'));
+          if (isDarkAlley) {
+            currentDaPage = newPage;
+            writeQueryParams({ dapage: currentDaPage });
+          } else {
+            currentPage = newPage;
+            writeQueryParams({ page: currentPage });
+          }
+          newSitesList.innerHTML = '<p style="display: flex; justify-content: center;"><img src="/icons/loading.svg" alt="loading" loading="lazy"/></p>';
+
+          fetchSites(token, type);
+        }
+      });
+
+      generateThumbnails();
+    }).catch(() => {
+      sitesList.innerHTML = '<p>Error loading sites</p>';
+    });
+}
+
+export default async function renderSites({ container, nav }) {
+  container.innerHTML = '<div class="sites"><p><img src="/icons/loading.svg" alt="loading" loading="lazy"/></p></div>';
+  nav.innerHTML = '<a href="/" id="create-new-button" title="Create new site" class="button primary action new">Create new site</a>';
+
+  await waitForAuthenticated();
+  const token = await window.auth0Client.getTokenSilently();
+
+  // List all sites (Dark Alley & Google Drive)
+  const fetchAllSites = () => {
+    fetchSites(token);
+    fetchSites(token, 'darkAlley');
+  };
+  const searchTerm = readQueryParams().search || '';
+  const sites = container.querySelector('.sites');
+  sites.innerHTML = `<input value="${searchTerm}" type="search" placeholder="Filter sites" class="filter-sites filter"><div class="sites-list"><section class="sites-list-dark-alley"></section><section class="sites-list-google-drive"></section></div>`;
+
+  document.querySelector('.filter-sites').oninput = (event) => {
+    if (event.target.value) {
+      writeQueryParams({ search: event.target.value });
+    } else {
+      removeQueryParams(['search']);
+    }
+    fetchAllSites();
+  };
+
+  fetchAllSites();
 }
---
+++ libs/pagination/pagination.js
@@ -0,0 +1,60 @@
+const loadCssFile = (path) => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = path;
+  // checks if the file is already loaded
+  if (document.querySelector(`link[href="${path}"]`)) {
+    return;
+  }
+  document.head.appendChild(link);
+};
+
+const paginator = (quantity, limit, page) => {
+  loadCssFile('/libs/pagination/pagination.css');
+  const pages = Math.ceil(quantity / limit);
+  if (pages <= 1) {
+    return '';
+  }
+
+  let currentPage = page;
+  let startPage;
+  let endPage;
+  let showFirstEllipsis;
+  let showSecondEllipsis;
+  let pageFilter;
+
+  const calculate = () => {
+    startPage = Math.max(currentPage - 2, 1);
+    endPage = Math.min(currentPage + 3, pages);
+    showFirstEllipsis = startPage > 3 && currentPage > 5;
+    showSecondEllipsis = endPage < pages - 2;
+    pageFilter = (p) => (p >= startPage && p <= endPage)
+        || (currentPage <= 5 && p <= currentPage);
+  };
+  calculate();
+
+  if (currentPage > pages) {
+    currentPage = 1;
+    calculate();
+  }
+
+  return `
+    <div class="pagination-container" data-current-page="${currentPage}">
+      ${currentPage > 1 ? `<button data-change-to="${currentPage - 1}" class="paginator prev">prev</button>` : ''}
+      ${startPage >= 1 ? `<button data-change-to="1" class="paginator${currentPage === 1 ? ' active' : ''}">1</button>${showFirstEllipsis ? '<span class="paginator ellipsis">...</span>' : ''}` : ''}
+      ${Array.from({ length: pages }, (_, i) => i + 2).filter((p) => pageFilter(p)).map((p) => `<button data-change-to="${p}" class="paginator${p === currentPage ? ' active' : ''}">${p}</button>`).join('')}
+      ${(() => {
+    if (showSecondEllipsis) {
+      return '<span class="paginator ellipsis">...</span>';
+    } if (endPage < pages - 1) {
+      return `<button data-change-to="${pages - 1}" class="paginator">${pages - 1}</button>`;
+    }
+    return '';
+  })()}
+      ${endPage < pages ? `<button data-change-to="${pages}" class="paginator">${pages}</button>` : ''}
+      ${currentPage !== pages ? `<button data-change-to="${currentPage + 1}" class="paginator next">next</button>` : ''}
+    </div>
+  `;
+};
+
+export default paginator;
