1b8e27d324aba8e70e319f67bdea7a803dfeee21
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2024-10-21 16:48:38 +0200

    updated anonymous table to be more readable

--- blocks/dashboard/renderAdmin.js
+++ blocks/dashboard/renderAdmin.js
@@ -1,5 +1,97 @@
 import { OOPS, SCRIPT_API, waitForAuthenticated } from '../../scripts/scripts.js';
 import renderAnalytics from '../../scripts/analytics.js';
+import { toClassName } from '../../scripts/aem.js';
+
+const langNames = new Intl.DisplayNames(['en'], { type: 'language' });
+function parseAcceptLanguage(str) {
+  if (!str) return null;
+  // const mainLanguage =
+  return langNames.of(str.split(',')[0]);
+}
+
+// header: sec-ch-ua
+function parseBrowser(str) {
+  if (!str) return null;
+
+  const browserParts = str.split(',');
+  try {
+    let output = '';
+    for (const browserStr of browserParts) {
+      const browserName = browserStr.split(';')[0].trim().replaceAll('"', '');
+
+      if (/not\?a_brand/i.test(browserName)) continue;
+      if (/chromium/i.test(browserName)) {
+        output = browserName;
+        continue;
+      }
+
+      if (browserName) {
+        output = browserName;
+        break;
+      }
+    }
+
+    return output;
+  } catch (error) {
+    return null;
+  }
+}
+
+function createTable({ headers, rows, tableClass = '' }) {
+  const columns = [];
+  const table = document.createElement('table');
+  table.className = tableClass;
+
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+  table.append(thead, tbody);
+
+  const headRow = document.createElement('tr');
+  thead.append(headRow);
+  for (const header of headers) {
+    const th = document.createElement('th');
+    th.textContent = header;
+    const columnFromHeader = toClassName(header);
+    th.dataset.column = columnFromHeader;
+    columns.push(columnFromHeader);
+    headRow.append(th);
+  }
+
+  for (const row of rows) {
+    const tr = document.createElement('tr');
+    for (const column of columns) {
+      const td = document.createElement('td');
+      const rowColumnItem = row[column];
+      if (!rowColumnItem) {
+        tr.append(td);
+        continue;
+      }
+      td.dataset.column = column;
+      if (typeof rowColumnItem === 'string') {
+        td.textContent = rowColumnItem || '';
+      } else {
+        const { link, value, title } = rowColumnItem;
+        let textContainer = td;
+        // add link
+        if (link) {
+          const anchor = document.createElement('a');
+          anchor.href = link;
+          anchor.target = '_blank';
+          td.append(anchor);
+          textContainer = anchor;
+        }
+        // add title
+        if (title) td.title = title;
+        textContainer.textContent = value || '';
+      }
+      tr.append(td);
+    }
+    tbody.append(tr);
+  }
+
+  return { table, thead, tbody };
+}
 
 export default async function renderAdmin({ container, nav }) {
   nav.insertAdjacentHTML('beforeend', `
@@ -278,6 +370,7 @@ export default async function renderAdmin({ container, nav }) {
 
     if (reqAnonymous.ok) {
       const anonymous = await reqAnonymous.json();
+      console.log('anonymous:', anonymous);
       const ips = Object.keys(anonymous);
       const timestamps = {};
       ips.forEach((ip) => {
@@ -286,63 +379,127 @@ export default async function renderAdmin({ container, nav }) {
         });
       });
 
-      anonymousContainer.innerHTML = `
-        <input type="text" placeholder="Filter by IP" class="filter-anonymous filter">
-        <table class="anonymous">
-            <thead>
-                <th>IP</th>
-                <th>Event</th>
-                <th>Date</th>
-                <th>URL</th>
-                <th>Location</th>
-                <th>Referrer</th>
-                <th>Browser</th>
-                <th>Device</th>
-            </thead>
-            <tbody>
-              ${Object.keys(timestamps)
-    .sort((timestampA, timestampB) => new Date(Number(timestampB)) - new Date(Number(timestampA)))
-    .map(
-      (timestamp) => (['server api request', 'server page request', 'server redirect request'].includes(timestamps[timestamp].event) ? `
-                        <tr>
-                          <td>${timestamps[timestamp].ip}</td>
-                          <td>${timestamps[timestamp].event}</td>
-                          <td>${new Date(Number(timestamp)).toLocaleString()}</td>
-                          <td><a href="${timestamps[timestamp].url}" target="_blank">${timestamps[timestamp].url}</a></td>
-                          <td>${timestamps[timestamp].city ?? ''} ${timestamps[timestamp].country}</td>
-                          
-                          <td>${timestamps[timestamp].referrer ? `<a href="${timestamps[timestamp].referrer}" target="_blank">${timestamps[timestamp].referrer}</a>` : ''}</td>
-                          <td>${timestamps[timestamp].browser ?? ''} ${timestamps[timestamp].language ?? ''}</td>
-                          <td>${timestamps[timestamp].platform ?? ''}</td>
-                        </tr>
-                    ` : `
-                    <tr>
-                      <td>${timestamps[timestamp].ip}</td>
-                      <td>${timestamps[timestamp].event}${timestamps[timestamp].isSPA ? ' SPA' : ''}</td>
-                      <td>${new Date(Number(timestamp)).toLocaleString()}</td>
-                      <td><a href="${timestamps[timestamp].url}" target="_blank">${
-          timestamps[timestamp].url
-        }</a></td>
-                                  <td>${timestamps[timestamp].location.city} - ${timestamps[timestamp].location.country}</td>
-                                  
-                                  <td>${
-        timestamps[timestamp].referrer
-          ? `<a href="${timestamps[timestamp].referrer}" target="_blank">${timestamps[timestamp].referrer}</a>`
-          : ''
-        }</td>
-                                  <td>${timestamps[timestamp].userAgent.browser.name} ${
-          timestamps[timestamp].userAgent.browser.version
-        } ${timestamps[timestamp].language}</td>
-                                  <td>${timestamps[timestamp].userAgent.device?.vendor ?? ''} ${
-          timestamps[timestamp].userAgent.os.name
-        } ${timestamps[timestamp].userAgent.os.version}</td>
-                                </tr>
-                              `),
-    )
-    .join('')}
-            </tbody>
-        </table>
-      `;
+
+
+      const anonymousTable = createTable({
+        headers: ['IP', 'Event', 'Date', 'URL', 'Location', 'Referrer', 'Browser', 'Device'],
+        rows: Object.keys(timestamps)
+          .sort((timestampA, timestampB) => new Date(Number(timestampB)) - new Date(Number(timestampA)))
+          .map((timestamp) => {
+            const serverEvent = ['server api request', 'server page request', 'server redirect request'].includes(timestamps[timestamp].event);
+
+            return {
+              ip: timestamps[timestamp].ip,
+              event: timestamps[timestamp].event + (!serverEvent && timestamps[timestamp].isSPA ? ' SPA' : ''),
+              date: {
+                value: new Date(Number(timestamp)).toLocaleString({
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                }),
+                title: new Date(Number(timestamp)).toLocaleString(),
+              },
+              url: {
+                value: timestamps[timestamp].url.replace(SCRIPT_API, ''),
+                title: timestamps[timestamp].url,
+              },
+              location: [timestamps[timestamp].city, timestamps[timestamp].country].filter(Boolean).join(', '),
+              referrer: {
+                value: timestamps[timestamp].referrer,
+                link: timestamps[timestamp].referrer,
+              },
+              browser: !serverEvent ? {
+                value: `${timestamps[timestamp].userAgent.browser.name} ${
+                  timestamps[timestamp].userAgent.browser.version
+                } ${timestamps[timestamp].language}`,
+              } : {
+                title: [
+                  timestamps[timestamp].browser ? `Full sec-ch-ua header: ${timestamps[timestamp].browser.replaceAll('"', '&quot;')}` : null,
+                  timestamps[timestamp].language ? `Full accept-language header: ${timestamps[timestamp].language.replaceAll('"', '&quot;')}` : null,
+                ].filter(Boolean).join('\n'),
+                value: [
+                  parseBrowser(timestamps[timestamp].browser),
+                  parseAcceptLanguage(timestamps[timestamp].language),
+                ].filter(Boolean).join(', '),
+              },
+              device: !serverEvent ? `${
+                timestamps[timestamp].userAgent.device?.vendor} ${
+                timestamps[timestamp].userAgent.os.name} ${
+                timestamps[timestamp].userAgent.os.version
+              }` : timestamps[timestamp]?.device?.replaceAll('"', ''),
+            };
+          }),
+      });
+
+      anonymousContainer.innerHTML = '<input type="text" placeholder="Filter by IP" class="filter-anonymous filter">';
+      anonymousContainer.append(anonymousTable.table);
+
+      //       anonymousContainer.innerHTML = `
+      //         <input type="text" placeholder="Filter by IP" class="filter-anonymous filter">
+      //         <table class="anonymous">
+      //             <thead>
+      //                 <th data-column="ip">IP</th>
+      //                 <th data-column="event">Event</th>
+      //                 <th data-column="date">Date</th>
+      //                 <th data-column="url">URL</th>
+      //                 <th data-column="location">Location</th>
+      //                 <th data-column="referrer">Referrer</th>
+      //                 <th data-column="browser">Browser</th>
+      //                 <th data-column="device">Device</th>
+      //             </thead>
+      //             <tbody>
+      //               ${Object.keys(timestamps)
+      //     .sort((timestampA, timestampB) => new Date(Number(timestampB)) - new Date(Number(timestampA)))
+      //     .map(
+      //       (timestamp) => (['server api request', 'server page request', 'server redirect request'].includes(timestamps[timestamp].event) ? `
+      //     <tr>
+      //       <td data-column="ip">${timestamps[timestamp].ip}</td>
+      //       <td data-column="event">${timestamps[timestamp].event}</td>
+      //       <td data-column="date">${new Date(Number(timestamp)).toLocaleString()}</td>
+      //       <td data-column="url"><a href="${timestamps[timestamp].url}" target="_blank">${timestamps[timestamp].url}</a></td>
+      //       <td data-column="location">${timestamps[timestamp].city ?? ''} ${timestamps[timestamp].country}</td>
+
+      //       <td data-column="referrer">${timestamps[timestamp].referrer ? `<a href="${timestamps[timestamp].referrer}" target="_blank">${timestamps[timestamp].referrer}</a>` : ''}</td>
+
+      //       <td data-column="browser" title="${
+      //         timestamps[timestamp].browser ? `Full sec-ch-ua header: ${timestamps[timestamp].browser.replaceAll('"', '&quot;')}` : ''}
+
+      // ${timestamps[timestamp].language ? `Full accept-language header: ${timestamps[timestamp].language.replaceAll('"', '&quot;')}` : ''}
+      // ">${parseBrowser(timestamps[timestamp].browser) ?? ''}${timestamps[timestamp].language && timestamps[timestamp].browser ? ', ' : ''}
+      //     ${parseAcceptLanguage(timestamps[timestamp].language) ?? ''}</td>
+
+      //     <td data-column="device">${timestamps[timestamp].platform?.replace(/"/g, '') ?? ''}</td>
+      //   </tr>
+      // ` : `
+      // <tr>
+      // <td>${timestamps[timestamp].ip}</td>
+      // <td>${timestamps[timestamp].event}${timestamps[timestamp].isSPA ? ' SPA' : ''}</td>
+      // <td>${new Date(Number(timestamp)).toLocaleString()}</td>
+      // <td><a href="${timestamps[timestamp].url}" target="_blank">${
+      //           timestamps[timestamp].url
+      //         }</a></td>
+      //                                   <td>${timestamps[timestamp].location.city} - ${timestamps[timestamp].location.country}</td>
+
+      //                                   <td>${
+      //         timestamps[timestamp].referrer
+      //           ? `<a href="${timestamps[timestamp].referrer}" target="_blank">${timestamps[timestamp].referrer}</a>`
+      //           : ''
+      //         }</td>
+      //                                   <td>${timestamps[timestamp].userAgent.browser.name} ${
+      //           timestamps[timestamp].userAgent.browser.version
+      //         } ${timestamps[timestamp].language}</td>
+      //                                   <td>${timestamps[timestamp].userAgent.device?.vendor ?? ''} ${
+      //           timestamps[timestamp].userAgent.os.name
+      //         } ${timestamps[timestamp].userAgent.os.version}</td>
+      //                                 </tr>
+      //                               `),
+      //     )
+      //     .join('')}
+      //             </tbody>
+      //         </table>
+      //       `;
 
       const filterAnonymous = anonymousContainer.querySelector('.filter-anonymous');
       filterAnonymous.oninput = () => {
