0fcbd90c258c8b1a9ace26ebd132fd38c20cb80a
Fankhauser-Dominik <domi.fankhauser@hotmail.com>
2025-01-29 18:24:34 +0100

    Overflow Fix for Anonymous Activities

--- blocks/site-details/site-details.js
+++ blocks/site-details/site-details.js
@@ -19,6 +19,7 @@ import renderSettingsGeneral from './renderSettingsGeneral.js';
 import renderSiteAnalytics from './renderSiteAnalytics.js';
 import renderSiteOverview from './renderSiteOverview.js';
 import renderSitePages from './renderSitePages.js';
+import renderSiteSpreadsheets from './renderSiteSpreadsheets.js';
 import renderSiteSEO from './renderSiteSEO.js';
 
 /**
@@ -83,6 +84,12 @@ export default async function decorate(block) {
         href: `${pathname}/pages`,
         iconSrc: '/icons/web.svg',
         renderTab: renderSitePages,
+      },
+      {
+        name: 'Spreadsheets',
+        href: `${pathname}/sheets`,
+        iconSrc: '/icons/web.svg',
+        renderTab: renderSiteSpreadsheets,
       },
       {
         name: 'SEO',
